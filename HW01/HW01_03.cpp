702ee759880942ce5afe27aab89f100fff0eda77
Lxxig <mayong0707@gmail.com>
2025-09-08 13:35:03 +0900

    HW01_03 완료

---
+++ HW01/HW01_03.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using int64 = long long;
+
+int64 Power(int64 Base, int64 Exponent)
+{
+    int64 Result = 1;
+    
+    while (Exponent > 0)
+    {
+        if (Exponent % 2 == 1)
+        {
+            Result *= Base;
+        }
+
+        Base = Base * Base;
+        Exponent /= 2;
+    }
+
+    return Result;
+}
+
+int main()
+{
+    int64 Base, Exponent;
+
+    std::cin >> Base >> Exponent;
+    std::cout << Power(Base, Exponent) << "\n";
+    
+    return 0;
+}
